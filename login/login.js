1dc32e1dedafce8dbe97b3abd13162154d4e0061
codot-spec <exform.arc@gmail.com>
2024-12-14 20:49:50 -0800

    update3

---
+++ login/login.js
@@ -0,0 +1,16 @@
+async function SignIn(event){
+    try{
+      event.preventDefault();
+      console.log(event.target.email.value);
+      const loginDetails = {
+        email: event.target.email.value,
+        password: event.target.password.value,
+      };
+    console.log(loginDetails);
+      const response = await  axios.post("http://localhost:3000/user/sign-in", loginDetails)
+        alert(response.data.message)
+    }
+    catch(err){
+      document.body.innerHTML += `<div style="color:red;">${err.message} <div> `
+    }
+}
